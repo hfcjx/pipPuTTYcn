cbac8cce7d11e9e4612ef648c5d66bdd30205778
hfcjx <hfcjx@126.com>
2024-11-19 14:22:23 +0800

    PuTTY(便携中文)的分支

---
+++ SRC/version.h
@@ -0,0 +1,15 @@
+/*
+ * This header file provides the various versioning-related #defines
+ * for a particular PuTTY build.
+ *
+ * When my automated build system does a full build, Buildscr
+ * completely overwrites this file with information derived from the
+ * circumstances and type of that build. The information _here_ is
+ * default stuff used for local development runs of 'make'.
+ */
+//PuTTYAttach
+//#define TEXTVER "发布版 0.81-p23-cn1 (便携版 0.23)"
+#define TEXTVER "(pipPuTTY)发布版 0.81-p23-cn1 (便携版 0.23)"
+//
+#define SSHVER "-Release-0-81-Patch23-CN1"
+#define BINARY_VERSION 0,81,23,1
---
+++ SRC/windows/utils/pgp_fingerprints_msgbox.c
@@ -0,0 +1,25 @@
+/*
+ * Display the fingerprints of the PGP Master Keys to the user as a
+ * GUI message box.
+ */
+
+#include "putty.h"
+
+void pgp_fingerprints_msgbox(HWND owner)
+{
+    message_box(
+        owner,
+        "这些是 PuTTY PGP 主密钥指纹。\n"
+        "它们可用于建立从此可执行文件到另一个可执行文件的信任路径。\n"
+        "有关详细信息请参阅手册。\n"
+        "(注意：这些指纹与 SSH 无关！)\n"
+        "\n"
+        "PuTTY 主密钥 " PGP_MASTER_KEY_YEAR
+        " (" PGP_MASTER_KEY_DETAILS "):\n"
+        "  " PGP_MASTER_KEY_FP "\n\n"
+        "上级主密钥 (" PGP_PREV_MASTER_KEY_YEAR
+        ", " PGP_PREV_MASTER_KEY_DETAILS "):\n"
+        "  " PGP_PREV_MASTER_KEY_FP,
+        "PGP 指纹", MB_ICONINFORMATION | MB_OK,
+        HELPCTXID(pgp_fingerprints));
+}
